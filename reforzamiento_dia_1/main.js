60f2c44b4a98695aff6c85661288471d32bd1b32
BryanJoseM79 <bryanjosem79@gmail.com>
2021-01-04 23:33:35 -0300

    2 funciones creadas, fonos y tabla

--- reforzamiento_dia_1/main.js
+++ reforzamiento_dia_1/main.js
@@ -1,4 +1,17 @@
 function fonos(){
     var telefono = document.getElementById('fono').value;
-    
+        if(telefono == ""){
+            document.getElementById('fono').setAttribute('value', '569');
+        }
+}
+
+function tabla(){
+    var direc = document.getElementById('direcciones').value;
+        for(let i = 1 ; i <= direc ; i++){
+            var add = document.createElement('input');
+            add.setAttribute('type', 'text');
+            add.setAttribute('id','direccion'+i); // <input type="text" id="direccion1 (con el +i irá creando direccion2, direccion3 etc)">
+            add.setAttribute('placeholder', 'Dirección');
+            document.getElementById('dirs').appendChild(add);
+        }
 }

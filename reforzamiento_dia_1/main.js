caeae95235089622efc018143414dd117cbc6534
BryanJoseM79 <bryanjosem79@gmail.com>
2021-01-05 01:35:17 -0300

    Formulario creado con aliación 569 al hacer click sobre el input, tambien implementado la eliminación de un div, aparte al seleccionar cuantas direcciones tiene se le despliegan la cantidad indicada en inputs

--- reforzamiento_dia_1/main.js
+++ reforzamiento_dia_1/main.js
@@ -7,11 +7,19 @@ function fonos(){
 
 function tabla(){
     var direc = document.getElementById('direcciones').value;
+    
         for(let i = 1 ; i <= direc ; i++){
-            var add = document.createElement('input');
-            add.setAttribute('type', 'text');
-            add.setAttribute('id','direccion'+i); // <input type="text" id="direccion1 (con el +i irá creando direccion2, direccion3 etc)">
-            add.setAttribute('placeholder', 'Dirección');
-            document.getElementById('dirs').appendChild(add); 
+            var add = document.createElement('input'); 
+                add.setAttribute('type', 'text');
+                add.setAttribute('id','direccion'+i); // <input type="text" id="direccion1 (con el +i irá creando direccion2, direccion3 etc)">
+                add.setAttribute('placeholder', 'Dirección');
+            // ------------------------------------------ //
+            var division = document.createElement('div');
+                
+                division.appendChild(add);
+
+                document.getElementById('dirs').appendChild(division); 
         }
+    var divisor = document.getElementById('des');
+    divisor.parentNode.removeChild(divisor);
 }

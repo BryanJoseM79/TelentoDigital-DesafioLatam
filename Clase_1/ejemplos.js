9f68c17e012824e81255bb7d8042bbdb3fc6c4b2
BryanJoseM79 <bryanjosem79@gmail.com>
2021-01-09 21:41:50 -0300

    Ediciones en clase 1 y clase 3

---
+++ Clase_1/ejemplos.js
@@ -0,0 +1,10 @@
+class Animal {
+    constructor(name){
+        this._name = name;
+    }
+    get name() {
+        return this._name;
+    }
+}
+   const animal = new Animal('Kratos');
+   console.log(animal.name);
